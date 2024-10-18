c2c7c172eb9f49eb492237be8cc76d5b36d2444b
Haffssaa <hm573@kent.ac.uk>
2024-10-18 10:33:57 +0000

    part1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,11 @@
+name = input( "Enter a name")
 
+snake_case = ""
+
+for c in name:
+     if c.islower():
+        print (c,end="")
+     if c.isupper() : 
+        print ("_" , c.lower(),end="",sep="")
+
+print()
